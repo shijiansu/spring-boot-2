5a92da8a7bd6fe1ff126e98038cf74db99821fdf
Shijian Su <sushijian.iss.nus@gmail.com>
2020-08-16 11:03:15 +0800

    Add SpringBoot microservice examples

---
+++ -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/book/src/main/java/com/brownfield/pss/book/component/BookingComponent.java
@@ -0,0 +1,120 @@
+package com.brownfield.pss.book.component;
+
+import com.brownfield.pss.book.entity.BookingRecord;
+import com.brownfield.pss.book.entity.Inventory;
+import com.brownfield.pss.book.entity.Passenger;
+import com.brownfield.pss.book.repository.BookingRepository;
+import com.brownfield.pss.book.repository.InventoryRepository;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class BookingComponent {
+  private static final String FareURL = "http://localhost:8082";
+
+  BookingRepository bookingRepository;
+  InventoryRepository inventoryRepository;
+
+  // @Autowired
+  //	private RestTemplate restTemplate;
+  private WebClient webClient;
+
+  Sender sender;
+
+  @Autowired
+  public BookingComponent(
+      BookingRepository bookingRepository, Sender sender, InventoryRepository inventoryRepository) {
+    this.bookingRepository = bookingRepository;
+    // this.restTemplate = new RestTemplate();
+    this.webClient = WebClient.create(FareURL);
+    this.sender = sender;
+    this.inventoryRepository = inventoryRepository;
+  }
+
+  public long book(BookingRecord record) {
+    log.info("calling fares to get fare");
+    // call fares to get fare
+    validateFareReactively(record);
+
+    // check fare
+    log.info("calling inventory to get inventory");
+
+    // check inventory
+    Inventory inventory =
+        inventoryRepository.findByFlightNumberAndFlightDate(
+            record.getFlightNumber(), record.getFlightDate());
+    if (!inventory.isAvailable(record.getPassengers().size())) {
+      throw new BookingException("No more seats available");
+    }
+    log.info("successfully checked inventory {}", inventory);
+    log.info("calling inventory to update inventory");
+
+    // update inventory
+    inventory.setAvailable(inventory.getAvailable() - record.getPassengers().size());
+    inventoryRepository.saveAndFlush(inventory);
+    log.info("successfully updated inventory");
+
+    // save booking
+    record.setStatus(BookingStatus.BOOKING_CONFIRMED);
+    Set<Passenger> passengers = record.getPassengers();
+    passengers.forEach(passenger -> passenger.setBookingRecord(record));
+    record.setBookingDate(new Date());
+    long id = bookingRepository.save(record).getId();
+    log.info("Successfully saved booking {}", id);
+
+    // send a message to search to update inventory
+    log.info("sending a booking event");
+    Map<String, Object> bookingDetails = new HashMap<>();
+    bookingDetails.put("FLIGHT_NUMBER", record.getFlightNumber());
+    bookingDetails.put("FLIGHT_DATE", record.getFlightDate());
+    bookingDetails.put("NEW_INVENTORY", inventory.getBookableInventory());
+
+    sender.send(bookingDetails);
+
+    log.info("booking event successfully delivered {}", bookingDetails);
+    return id;
+  }
+
+  public BookingRecord getBooking(long id) {
+    return bookingRepository.findById(id).orElse(null);
+  }
+
+  public void updateStatus(String status, long bookingId) {
+    bookingRepository
+        .findById(bookingId)
+        .ifPresentOrElse(
+            b -> b.setStatus(status), () -> log.info("Booking does not exist {}", bookingId));
+  }
+
+  public void validateFareReactively(BookingRecord record) {
+    // Fare fare = restTemplate.getForObject(FareURL
+    // +"/get?flightNumber="+record.getFlightNumber()+"&flightDate="+record.getFlightDate(),Fare.class);
+    Mono<Fare> result =
+        webClient
+            .get()
+            .uri(
+                "/fares/get?flightNumber="
+                    + record.getFlightNumber()
+                    + "&flightDate="
+                    + record.getFlightDate())
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .flatMap(response -> response.bodyToMono(Fare.class));
+    // It is not to give back Mono, so subscribe here for the further process
+    result.subscribe(fare -> checkFare(record.getFare(), fare.getFare()));
+  }
+
+  private void checkFare(String requestedFare, String actualFare) {
+    log.info("calling fares to get fare (reactively collected) {}", actualFare);
+    if (!requestedFare.equals(actualFare)) throw new BookingException("Fare is tampered");
+  }
+}
---
+++ -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/website/src/main/java/com/brownfield/pss/client/Application.java
@@ -0,0 +1,80 @@
+package com.brownfield.pss.client;
+
+import com.brownfield.pss.client.component.BookingRecord;
+import com.brownfield.pss.client.component.CheckInRecord;
+import com.brownfield.pss.client.component.Flight;
+import com.brownfield.pss.client.component.Passenger;
+import com.brownfield.pss.client.component.SearchQuery;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@Slf4j
+public class Application implements CommandLineRunner {
+  RestTemplate searchClient = new RestTemplate();
+
+  RestTemplate bookingClient = new RestTemplate();
+
+  RestTemplate checkInClient = new RestTemplate();
+
+  // RestTemplate restClient= new RestTemplate();
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  public void run(String... strings) {
+    // Search for a flight
+    SearchQuery searchQuery = new SearchQuery("NYC", "SFO", "22-JAN-18");
+    Flight[] flights =
+        searchClient.postForObject("http://localhost:8083/search/get", searchQuery, Flight[].class);
+
+    Arrays.asList(flights).forEach(flight -> log.info(" flight >" + flight));
+
+    // create a booking only if there are flights.
+    if (flights.length == 0) {
+      return;
+    }
+    Flight flight = flights[0];
+    BookingRecord booking =
+        new BookingRecord(
+            flight.getFlightNumber(),
+            flight.getOrigin(),
+            flight.getDestination(),
+            flight.getFlightDate(),
+            null,
+            flight.getFares().getFare());
+    Set<Passenger> passengers = new HashSet<>();
+    passengers.add(new Passenger("Gavin", "Franc", "Male", booking));
+    booking.setPassengers(passengers);
+    long bookingId = 0;
+    try {
+      bookingId =
+          bookingClient.postForObject("http://localhost:8080/booking/create", booking, long.class);
+      log.info("Booking created {}", bookingId);
+    } catch (Exception e) {
+      log.error("BOOKING SERVICE NOT AVAILABLE...!!!");
+    }
+
+    // check in passenger
+    if (bookingId == 0) return;
+    try {
+      CheckInRecord checkIn =
+          new CheckInRecord("Franc", "Gavin", "28C", null, "BF101", "22-JAN-18", bookingId);
+      long checkinId =
+          checkInClient.postForObject("http://localhost:8081/checkin/create", checkIn, long.class);
+      log.info("Checked IN {}", checkinId);
+    } catch (Exception e) {
+      log.error("CHECK IN SERVICE NOT AVAILABLE...!!!");
+    }
+  }
+}
