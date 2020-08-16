bc13389ee8c84ab8c7e39f6dd2f3a0bf3429df16
Shijian Su <sushijian.iss.nus@gmail.com>
2020-08-16 10:52:30 +0800

    Add project structure, and boot up scripts

---
+++ -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/README.md
@@ -0,0 +1,9 @@
+
+```bash
+# maunally run the microservice one by one
+run-manually.sh
+
+# run or stop all in one
+run.sh
+run-stop.sh
+```
---
+++ -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/run.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+function _check_ports {
+  p=$(lsof -i :8083 -i :8082 -i :8080 -i :8081 -i :8001)
+  echo "${p}"
+
+  if [[ -n "${p}" ]]; then
+    echo "[ERROR] Microservices ports are occupied, please manually kill the processor(s)"
+    exit 1
+  fi
+}
+
+function _remove_log {
+  local application=${1}
+  [[ -f "_log/${application}.log" ]] && rm "_log/${application}.log"
+}
+
+function _check_rabbitmq_status {
+  # -q: exit immediately with zero status if any match is found
+  local container_name="rabbitmq"
+  while docker ps --filter "name=${container_name}" --filter status=running | grep -q "${container_name}"; do
+    result=$(docker logs ${container_name} 2>&1 | grep "completed with")
+    if [[ -n "${result}" ]]; then
+      echo "[Docker - RabbitMQ] Started"
+      break
+    else
+      echo "[Docker - RabbitMQ] Booting... ..."
+      sleep 2s
+    fi
+  done
+}
+
+function _check_spring_boot_status {
+  local application=${1}
+  local log="${2}"
+  echo ""
+  while [[ -f "${log}" ]]; do
+    if grep -q "Started Application" "${log}"; then
+      echo "[SpringBoot - ${application}] Started"
+      break
+    elif grep -q "Failed to execute goal" "${log}"; then
+      echo "[SpringBoot- ${application}] Failed tp start, more info at ${log}... ..."
+      exit 1
+    else
+      echo "[SpringBoot- ${application}] Booting... ..."
+      sleep 2s
+    fi
+  done
+}
+
+function run_spring_boot {
+  local application=${1}
+  local log="_log/${application}.log"
+  # resolve "WARNING: An illegal reflective access operation has occurred"
+  mvn -f "${application}"/pom.xml clean spring-boot:run > "${log}" &
+  echo $! >> run.pid
+  _check_spring_boot_status "${application}" "${log}"
+}
+
+# if use docker compose, it can use "depends on" to ensure application is up
+
+# ports are available
+_check_ports
+
+# logging
+[[ ! -d _log ]] && mkdir _log
+for app in "book" "checkin" "fares" "rabbitmq" "search" "website"; do
+  _remove_log ${app}
+done
+
+# rabbitmq
+docker stop rabbitmq > /dev/null 2>&1
+docker rm rabbitmq > /dev/null 2>&1
+docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.8.6-management > "_log/rabbitmq.log" # detached mode
+_check_rabbitmq_status
+
+# microservices
+[[ -f run.pid ]] && rm run.pid
+
+run_spring_boot "search" # 8083
+run_spring_boot "fares" # 8082
+run_spring_boot "book" # 8080
+run_spring_boot "checkin" # 8081
+run_spring_boot "website" # 8001
+
+open http://localhost:8001
