1915184f24a98342043fac5c4b29341db4fbe7c6
Shijian Su <sushijian.iss.nus@gmail.com>
2020-08-19 15:26:09 +0800

    Update the env only for the example, not combining the Maven and Gradle

--- spring-boot-2-dockerizing/1_spring-boot-2-docker/env.sh
+++ spring-boot-2-dockerizing/1_spring-boot-2-docker/env.sh
@@ -1,35 +1,8 @@
 #!/bin/bash
-ARTIFACT_ID="spring-boot-2-docker"
-export ARTIFACT_ID
-
-VERSION="0.0.1-SNAPSHOT"
-export VERSION
-
-# docker name
-DOCKER_NAME="${ARTIFACT_ID}"
-export DOCKER_NAME
-
-# image name
-## shijiansu is user name of hub.docker.com
-IMAGE_NAME="shijiansu/${DOCKER_NAME}:${VERSION}"
-export IMAGE_NAME
-
-# package folder
-if [[ -f pom.xml ]]; then
-  PACKAGE_FOLDER="target" # maven project
-elif [[ -f build.gradle ]]; then
-  PACKAGE_FOLDER="build" # gradle project
-else
-  PACKAGE_FOLDER="target" # deafult case
-fi
-export PACKAGE_FOLDER
-
-# if there is lib folder
-if [[ -f pom.xml ]]; then
-  LIB_FOLDER="" # maven project
-elif [[ -f build.gradle ]]; then
-  LIB_FOLDER="libs/" # gradle project
-else
-  LIB_FOLDER="" # default
-fi
-export LIB_FOLDER
+export ARTIFACT_ID="spring-boot-2-docker"
+export VERSION="0.0.1-SNAPSHOT"
+export DOCKER_NAME="${ARTIFACT_ID}"
+export IMAGE_NAME_PREFIX="shijiansu" # shijiansu is user name of hub.docker.com
+export IMAGE_NAME="${IMAGE_NAME_PREFIX}/${DOCKER_NAME}:${VERSION}" # image name
+export PACKAGE_FOLDER="target" # package folder - maven project
+export LIB_FOLDER="" # if there is lib folder - maven project
--- spring-boot-2-dockerizing/3_spring-boot-2-docker-spotify/env.sh
+++ spring-boot-2-dockerizing/3_spring-boot-2-docker-spotify/env.sh
@@ -1,38 +1,9 @@
 #!/bin/bash
-ARTIFACT_ID="spring-boot-2-docker-spotify"
-export ARTIFACT_ID
+export ARTIFACT_ID="spring-boot-2-docker-spotify"
+export VERSION="0.0.1-SNAPSHOT"
+export DOCKER_NAME="${ARTIFACT_ID}"
+export IMAGE_NAME_PREFIX="shijiansu" # shijiansu is user name of hub.docker.com
+export IMAGE_NAME="${IMAGE_NAME_PREFIX}/${DOCKER_NAME}:${VERSION}" # image name
+export PACKAGE_FOLDER="target" # package folder - maven project
+export LIB_FOLDER="" # if there is lib folder - maven project
 
-VERSION="0.0.1-SNAPSHOT"
-export VERSION
-
-# docker name
-DOCKER_NAME="${ARTIFACT_ID}"
-export DOCKER_NAME
-
-## shijiansu is user name of hub.docker.com
-IMAGE_NAME_PREFIX="shijiansu"
-export IMAGE_NAME_PREFIX
-
-# image name
-IMAGE_NAME="${IMAGE_NAME_PREFIX}/${DOCKER_NAME}:${VERSION}"
-export IMAGE_NAME
-
-# package folder
-if [[ -f pom.xml ]]; then
-  PACKAGE_FOLDER="target" # maven project
-elif [[ -f build.gradle ]]; then
-  PACKAGE_FOLDER="build" # gradle project
-else
-  PACKAGE_FOLDER="target" # deafult case
-fi
-export PACKAGE_FOLDER
-
-# if there is lib folder
-if [[ -f pom.xml ]]; then
-  LIB_FOLDER="" # maven project
-elif [[ -f build.gradle ]]; then
-  LIB_FOLDER="libs/" # gradle project
-else
-  LIB_FOLDER="" # deafult case
-fi
-export LIB_FOLDER
