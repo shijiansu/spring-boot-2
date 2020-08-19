1b3eee10e75e9b79cd5fe65434cf0777b447c709
Shijian Su <sushijian.iss.nus@gmail.com>
2020-08-19 15:05:19 +0800

    Update typo

--- spring-boot-2-dockerizing/1_spring-boot-2-docker/src/main/java/spring/boot/_2/docker/HelloController.java
+++ spring-boot-2-dockerizing/1_spring-boot-2-docker/src/main/java/spring/boot/_2/docker/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
   @RequestMapping("/")
-  public String home() {
+  public String hello() {
     return "Hello Docker World";
   }
 }
