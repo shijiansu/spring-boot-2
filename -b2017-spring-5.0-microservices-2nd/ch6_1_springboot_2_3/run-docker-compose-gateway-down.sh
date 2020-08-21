7b35daf0df18cffef37832517546fd672ad3e4fe
Shijian Su <sushijian.iss.nus@gmail.com>
2020-08-21 22:23:56 +0800

    Add docker compose with gateway scripts

--- -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/run-docker-compose-gateway-down.sh
+++ -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/run-docker-compose-gateway-down.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose down
+docker-compose -f docker-compose-gateway.yml down
--- -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/run-docker-compose-gateway.sh
+++ -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/run-docker-compose-gateway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/bin/bash run-docker-compose-down.sh
+/bin/bash run-docker-compose-gateway-down.sh
 
 function docker_build {
   local application=${1}
@@ -13,10 +13,14 @@ docker_build "checkin"
 docker_build "fares"
 docker_build "search"
 docker_build "website"
+docker_build "microservice-gateway"
 
-docker-compose up -d --remove-orphans
+docker-compose -f docker-compose-gateway.yml up -d --remove-orphans
 docker-compose ps
 
-docker logs -f website
+echo ""
+# docker logs -f website
+# docker logs -f book-microservice
+docker logs -f microservice-gateway
 
 # open http://localhost:8001
