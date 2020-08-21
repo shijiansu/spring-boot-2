78fe73ef89291f1afec4cecdca474946af7ab8aa
Shijian Su <sushijian.iss.nus@gmail.com>
2020-08-21 22:24:23 +0800

    Update clean up

--- -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/run-cleanup-all.sh
+++ -b2017-spring-5.0-microservices-2nd/ch6_1_springboot_2_3/run-cleanup-all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-/bin/bash run-docker-all-stop.sh
+/bin/bash run-all-stop.sh
 /bin/bash run-docker-compose-down.sh
+/bin/bash run-docker-compose-gateway-down.sh
 
-docker image rm shijian/springboot2-docker
-docker image rm shijian/springboot2-docker-client
+docker image rm shijian/book-microservice
+docker image rm shijian/checkin-microservice
+docker image rm shijian/fares-microservice
+docker image rm shijian/search-microservice
+docker image rm shijian/website
+docker image rm shijian/microservice-gateway
